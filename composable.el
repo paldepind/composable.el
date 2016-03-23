f5cc5cc4e36eec4ad0ba1b0b05bf8a1f561676e0
paldepind <simonfv@gmail.com>
2016-03-23 18:05:11 +0100

    Refactor

--- composable.el
+++ composable.el
@@ -26,49 +26,66 @@
 
 ;;; Code:
 
-(setq binders (make-hash-table :test 'equal))
+(defvar composable--binders (make-hash-table :test 'equal))
 
-(defun add-motions (&rest bs)
+(defun composable-add-motions (&rest bs)
+  "Add motions.
+BS must be pairs of a keybinding and a function."
   (dolist (b bs)
-    (puthash (kbd (car b)) (cadr b) binders)))
+    (puthash (kbd (car b)) (cadr b) composable--binders)))
 
-(defun composable--def-arg (arg) (if (zerop arg) 1 arg))
+(defun composable--def-arg (arg)
+  "Return 1 if ARG is 0 otherwise ARG."
+  (if (zerop arg) 1 arg))
 
 (defun composable--is-string-num-p (s)
+  "Return t if S begins with a number."
   (string-match-p "^[0-9]+" s))
 
 (defun composable--execute-action (command arg key)
+  "Execute the action COMMAND with ARG in the region KEY."
   (save-excursion
     (push-mark nil nil t)
     (let ((current-prefix-arg (composable--def-arg arg)))
-      (call-interactively (gethash key binders)))
+      (call-interactively (gethash key composable--binders)))
     (call-interactively command)))
 
 (defun composable--read-keys (command arg)
+  "Read keys and then incoke COMMAND with ARG."
   (let ((key (read-key-sequence nil)))
     (if (composable--is-string-num-p key)
-        (composable--read-keys command (+ (* arg 10) (string-to-int key)))
+        (composable--read-keys command (+ (* arg 10) (string-to-number key)))
       (composable--execute-action command arg key))))
 
-(defun create-action (command)
+(defun composable-create-composable (command)
+  "Take a function and return it in a composable wrapper.
+The returned function will ask for a motion, mark the region it
+specifies and call COMMAND on the region."
   (lambda ()
     (interactive)
     (if mark-active
         (call-interactively command)
       (composable--read-keys command 0))))
 
-(defun add-actions (&rest as)
-  (dolist (a as)
-    (global-set-key (kbd (car a)) (create-action (cadr a)))))
+(defun composable-def (commands)
+  "Define composable function from a list COMMANDS.
+The list should contain functions operating on regions.
+For each function named foo a function name composable-foo is created."
+  (dolist (c commands)
+    (fset (intern (concat "composable-" (symbol-name c)))
+          (composable-create-composable c))))
 
 (defun composable-mark-line (arg)
-  "Marks ARG lines."
+  "Mark ARG lines."
   (interactive "p")
   (beginning-of-line)
   (push-mark nil nil t)
-  (dotimes (i arg) (forward-line)))
+  (dotimes (_ arg) (forward-line)))
 
-(add-motions
+(composable-def
+ '(kill-region kill-ring-save smart-comment-region))
+
+(composable-add-motions
  '("e" move-end-of-line)
  '("a" move-beginning-of-line)
  '("'" avy-goto-char-in-line)
@@ -80,10 +97,9 @@
  '("{" backward-paragraph)
  '("}" forward-paragraph))
 
-(add-actions
- '("C-w" kill-region)
- '("M-w" kill-ring-save)
- '("M-;" smart-comment-region))
+(global-set-key (kbd "C-w") 'composable-kill-region)
+(global-set-key (kbd "M-w") 'composable-kill-ring-save)
+(global-set-key (kbd "M-;") 'composable-smart-comment-region)
 
 (provide 'composable)
 
